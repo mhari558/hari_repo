280fdebd703918c6848cb0a53ef208d7c8cafaa8
hari.manda <hari.manda@fissionlabs.in>
2016-09-02 13:24:55 +0530

    rumtime permissions

--- OrderWater/app/src/main/java/resource/water/com/waterresourceapp/NavigationMainActivity.java
+++ OrderWater/app/src/main/java/resource/water/com/waterresourceapp/NavigationMainActivity.java
@@ -1,6 +1,7 @@
 package resource.water.com.waterresourceapp;
 
 import android.*;
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -9,6 +10,7 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -16,6 +18,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
@@ -33,6 +36,7 @@ import resource.water.com.waterresourceapp.activities.LogInActivity;
 import resource.water.com.waterresourceapp.fragments.HomeFragment;
 import resource.water.com.waterresourceapp.fragments.MyOrdersFragment;
 import resource.water.com.waterresourceapp.fragments.MyProfileFragment;
+import resource.water.com.waterresourceapp.model.PermissionsModel;
 import resource.water.com.waterresourceapp.util.Utils;
 
 /**
@@ -45,15 +49,16 @@ public class NavigationMainActivity extends CommonActivity implements Navigation
     private DrawerLayout drawerLayout;
     private Toolbar toolbar;
     private NavigationView navigationView;
+    private  List<PermissionsModel> permissionsList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_layout);
-
-       // checkPermissions();
+        checkPermissions();
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        navigationView = (NavigationView)findViewById(R.id.navigation_view);
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
         navigationView.setNavigationItemSelectedListener(this);
         initNavigationDrawer();
         if (savedInstanceState == null) {
@@ -93,14 +98,14 @@ public class NavigationMainActivity extends CommonActivity implements Navigation
     public void initNavigationDrawer() {
 
         View header = navigationView.getHeaderView(0);
-        TextView tv_email = (TextView)header.findViewById(R.id.tv_email);
+        TextView tv_email = (TextView) header.findViewById(R.id.tv_email);
         tv_email.setText("raj.amalw@learn2crack.com");
-        drawerLayout = (DrawerLayout)findViewById(R.id.drawer);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
 
-        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close){
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
 
             @Override
-            public void onDrawerClosed(View v){
+            public void onDrawerClosed(View v) {
                 super.onDrawerClosed(v);
             }
 
@@ -112,6 +117,7 @@ public class NavigationMainActivity extends CommonActivity implements Navigation
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
     }
+
     private void loadFragment(Fragment fragment) {
         drawerLayout.closeDrawers();
         FragmentManager fragmentManager = getSupportFragmentManager();
@@ -129,11 +135,9 @@ public class NavigationMainActivity extends CommonActivity implements Navigation
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
 
-                Utils.showProgressBar(NavigationMainActivity.this,"Please Wait...");
-                Backendless.UserService.logout(new AsyncCallback<Void>()
-                {
-                    public void handleResponse( Void response )
-                    {
+                Utils.showProgressBar(NavigationMainActivity.this, "Please Wait...");
+                Backendless.UserService.logout(new AsyncCallback<Void>() {
+                    public void handleResponse(Void response) {
 
                         Utils.hideProgressBar();
                         // user has been logged out.
@@ -141,8 +145,7 @@ public class NavigationMainActivity extends CommonActivity implements Navigation
                         startActivity(intent);
                     }
 
-                    public void handleFault( BackendlessFault fault )
-                    {
+                    public void handleFault(BackendlessFault fault) {
                         // something went wrong and logout failed, to get the error code call fault.getCode()
                     }
                 });
@@ -158,28 +161,23 @@ public class NavigationMainActivity extends CommonActivity implements Navigation
         }).create().show();
 
     }
+
     private void checkPermissions() {
 
-        int cameraPermission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA);
-        int storagePermission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        int locationPermission = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION);
-
-        List<String> permissions = new ArrayList<>();
-
-        if (cameraPermission != PackageManager.PERMISSION_GRANTED) {
-            permissions.add(android.Manifest.permission.CAMERA);
-        }
-
-        if (storagePermission != PackageManager.PERMISSION_GRANTED) {
-            permissions.add(android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        }
+        int locationPermission = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        permissionsList = new ArrayList<>();
+        ArrayList<String> list = new ArrayList<>();
 
         if (locationPermission != PackageManager.PERMISSION_GRANTED) {
-            permissions.add(android.Manifest.permission.ACCESS_FINE_LOCATION);
+            PermissionsModel permissionsModel2 = new PermissionsModel();
+            permissionsModel2.setMessage(getResources().getString(R.string.location));
+            permissionsModel2.setPermissions(Manifest.permission.ACCESS_FINE_LOCATION);
+            permissionsList.add(permissionsModel2);
+            list.add(Manifest.permission.ACCESS_FINE_LOCATION);
         }
 
-        if (!permissions.isEmpty()) {
-            ActivityCompat.requestPermissions(this, permissions.toArray(new String[permissions.size()]), REQUEST_CODE_PERMISSIONS);
+        if (!list.isEmpty()) {
+            ActivityCompat.requestPermissions(this, list.toArray(new String[list.size()]), REQUEST_CODE_PERMISSIONS);
         }
     }
 
@@ -188,10 +186,17 @@ public class NavigationMainActivity extends CommonActivity implements Navigation
 
         switch (requestCode) {
             case REQUEST_CODE_PERMISSIONS: {
+
                 for (int i = 0; i < permissions.length; i++) {
-                    if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
-                        Toast.makeText(this, getString(R.string.enable_permissions), Toast.LENGTH_LONG).show();
-                        finish();
+
+                   if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                                permissions[i])) {
+                            showPermissionDialog(requestCode,permissionsList.get(i).getMessage(), permissions[i]);
+                        }else {
+                            Toast.makeText(this, "else "+permissionsList.get(i).getMessage(), Toast.LENGTH_LONG).show();
+                        }
                     }
                 }
             }
@@ -203,4 +208,23 @@ public class NavigationMainActivity extends CommonActivity implements Navigation
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
+    private void showPermissionDialog(final int requestCode, String message, final String permission) {
+
+        new android.support.v7.app.AlertDialog.Builder(this)
+                .setMessage(message)
+                .setPositiveButton(getResources().getString(R.string.ok), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ActivityCompat.requestPermissions(NavigationMainActivity.this, new String[]{permission}, requestCode);
+                    }
+                })
+                .setNegativeButton(getResources().getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .create()
+                .show();
+    }
 }
---
+++ OrderWater/app/src/main/java/resource/water/com/waterresourceapp/model/PermissionsModel.java
@@ -0,0 +1,27 @@
+package resource.water.com.waterresourceapp.model;
+
+/**
+ * Created by hari on 1/9/16.
+ */
+
+public class PermissionsModel {
+
+    private String permissions;
+    private String message;
+
+    public String getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(String permissions) {
+        this.permissions = permissions;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
